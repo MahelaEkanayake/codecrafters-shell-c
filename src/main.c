adea416a7998166f6617434cc1664179002f78b2
MahelaEkanayake <mahelaekanayake.aurelius@gmail.com>
2026-08-23 17:02:15 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
@@ -10,16 +11,21 @@ int main(int argc, char *argv[]) {
 
   char buffer [6000];
   char command [6000];
+  char exit_command[] = "exit";
 
   while(true){
 
   	printf("$ ");
 
-  	fgets(buffer, sizeof(buffer), stdin);
-    sscanf(buffer, "%6000s", command );
+  	fgets(buffer, sizeof(buffer), stdin);     // Scans the command
+    sscanf(buffer, "%6000s", command );       // fget() + sscanf() instead of scanf()
 
-  	printf("%s: command not found\n", command);
-    fflush(stdout);
+    if(strncmp(command, exit_command, strlen(exit_command))==0){  // Break the loop if command is "exit"
+      break;
+    }
+
+  	printf("%s: command not found\n", command);   // TEMPORY: output command
+    fflush(stdout);  //    flush buffered output
   }
 
   return 0;
